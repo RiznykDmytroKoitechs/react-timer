401006f838910f1b50b973957f64d4ee2b24b21e
RiznykDmytroKoitechs <115988767+RiznykDmytroKoitechs@users.noreply.github.com>
2022-11-02 14:04:38 +0200

    Making code a bit prettier

--- timer/src/components/timer/Timer.js
+++ timer/src/components/timer/Timer.js
@@ -23,9 +23,9 @@ export default function Timer() {
       const hours = Math.floor((timerValRef.current % (3600 * 60)) / 3600);
       const minutes = Math.floor((timerValRef.current % 3600) / 60);
       const seconds = timerValRef.current % 60;
-      textRef.current.textContent = `${("0" + hours).slice(-2)}:${(
-        "0" + minutes
-      ).slice(-2)}:${("0" + seconds).slice(-2)}`;
+      textRef.current.textContent = `${("0" + hours).slice(-2)} :
+      ${("0" + minutes).slice(-2)} :
+      ${("0" + seconds).slice(-2)}`;
       resetButtonRef.current.disabled = false;
     }, 1000);
   };
@@ -40,12 +40,12 @@ export default function Timer() {
   const resetTimer = () => {
     resetButtonRef.current.disabled = true;
     timerValRef.current = 0;
-    textRef.current.textContent = "00:00:00";
+    textRef.current.textContent = "00 : 00 : 00";
   };
 
   return (
     <div className="timerDiv">
-      <h1 ref={textRef}>00:00:00</h1>
+      <h1 ref={textRef}>00 : 00 : 00</h1>
       <div className="buttonsDiv">
         <button ref={startButtonRef} onClick={startTimer}>
           Start
