9a35e770d08a4708628ee21257792b6d7960ae0a
RiznykDmytroKoitechs <115988767+RiznykDmytroKoitechs@users.noreply.github.com>
2022-11-07 12:18:26 +0200

    Optimizations

--- timer/src/components/timer/Timer.js
+++ timer/src/components/timer/Timer.js
@@ -3,52 +3,61 @@ import React, { useEffect, useState } from "react";
 export default function Timer() {
   const [isEnabled, setIsEnabled] = useState(false);
   const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState();
 
-  const hours = Math.floor((time % (3600 * 60)) / 3600);
-  const minutes = Math.floor((time % 3600) / 60);
-  const seconds = time % 60;
+  const hours = ("0" + Math.floor((time % (3600 * 60)) / 3600)).slice(-2) ;
+  const minutes = ("0" + Math.floor((time % 3600) / 60)).slice(-2);
+  const seconds = ("0" + time % 60).slice(-2);
 
   useEffect(() => {
+    clearInterval(timer);
     if (isEnabled) {
-      const interval = setTimeout(() => {
-        setTime(time + 1);
-      }, 1000);
-      return () => {
-        clearTimeout(interval);
-      };
+      setTimer(
+        setInterval(() => {
+          setTime((time)=>time + 1);
+        }, 1000)
+      )
+      
     }
-  }, [isEnabled, time]);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isEnabled]);
+
+  const enableTimer = () => {
+    setIsEnabled(true);
+  }
+
+  const disableTimer = () => {
+    setIsEnabled(false);
+  }
+
+  const resetTimer = () => {
+    setTime(0);
+  }
 
   return (
     <div className="timerDiv">
       <h1>
-        {("0" + hours).slice(-2)}:
-        {("0" + minutes).slice(-2)}:
-        {("0" + seconds).slice(-2)}
+        {hours}:{minutes}:{seconds}
       </h1>
       <div className="buttonsDiv">
         <button
           disabled={isEnabled}
-          onClick={() => {
-            setIsEnabled(true);
-          }}
+          onClick={enableTimer}
         >
           Start
         </button>
         <button
           disabled={!isEnabled}
-          onClick={() => {
-            setIsEnabled(false);
-          }}
+          onClick={disableTimer}
         >
           Stop
         </button>
         <button
           className="resetButton"
           disabled={!time}
-          onClick={() => {
-            setTime(0);
-          }}
+          onClick={resetTimer}
         >
           Reset
         </button>
